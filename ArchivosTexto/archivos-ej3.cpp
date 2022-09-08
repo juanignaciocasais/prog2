b9ce9dcfb7685c99bc382d205f0622130d1b03ac
Juan I. Casais <juanignaciocasais@gmail.com>
2022-09-07 22:59:00 -0300

    Archivo texto Ej 3

---
+++ ArchivosTexto/archivos-ej3.cpp
@@ -0,0 +1,9 @@
+/*
+Dado el siguiente dibujo de un registro, especifique la nomenclatura para acceder al registro y cada
+uno de sus campos, y las operaciones que se pueden realizar a nivel registro y a nivel campos. Defina otro
+registro del mismo tipo de datos.
+
+Legajo 8 dìgitos
+ApellidoNombre 25 caracteres
+Nota 0...10
+*/
