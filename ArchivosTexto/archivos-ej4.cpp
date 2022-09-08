7752995cf7df9b9267c5b14822792219a72e19dc
Juan I. Casais <juanignaciocasais@gmail.com>
2022-09-07 23:05:05 -0300

    Archivo texto Ej 4

---
+++ ArchivosTexto/archivos-ej4.cpp
@@ -0,0 +1,20 @@
+/*
+Dados los siguientes dibujos que corresponden a distintos tipos de registros realice 
+su declaración en C++, y especifique la nomenclatura para acceder al registro y 
+cada uno de sus campos.
+
+a)
+nombre char[20]
+fechaNacimiento 
+    dia  char (1...31)
+    mes  char (1...12)
+    anio short (4 digitos)
+b)
+legajo (8 digitos) 
+apellidoNombre 
+    apellido (  20 caracteres)
+    nombre      (20 caracteres)
+calificaciones
+    Nota1
+    Nota2
+    Nota3
